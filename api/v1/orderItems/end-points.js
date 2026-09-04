86bf129e686f74ed2f7c035fd4daeac33e3ab618
Srivasu215 <sri.vasu215@gmail.com>
2026-09-04 18:37:24 +0530

    v2 an v3 started for api

---
+++ api/v1/orderItems/end-points.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import funcFromshowAll from './showAll/controller.js';
+import funcFrominsertWithMeta from './insertWithMeta/controller.js';
+import funcFromdel from './del/controller.js';
+import funcFrommodify from './modify/controller.js';
+
+const tableName = "orderItems.json";
+const tablePath = "Data/orderItems.json";
+const configPath = "Config/Schemas/orderItems.json";
+
+const router = express.Router();
+
+router.get('/showAll', (req, res) => funcFromshowAll({ req, res, inTablePath: tablePath }));
+router.post('/insertWithMeta', express.json(), (req, res) => funcFrominsertWithMeta({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.delete('/del/:pk', (req, res) => funcFromdel({ req, res, inTablePath: tablePath }));
+router.put('/modify', express.json(), (req, res) => funcFrommodify({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+
+export { router };
